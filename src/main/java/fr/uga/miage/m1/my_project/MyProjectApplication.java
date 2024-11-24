276b17972afe0d057987c39d6b38bc295a437532
badzia <m.badzi@proton.me>
2024-11-24 01:26:39 +0100

    correction minneur

--- src/main/java/fr/uga/miage/m1/my_project/MyProjectApplication.java
+++ src/main/java/fr/uga/miage/m1/my_project/MyProjectApplication.java
@@ -4,6 +4,9 @@ import fr.uga.miage.m1.my_project.service.ServerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class MyProjectApplication {
