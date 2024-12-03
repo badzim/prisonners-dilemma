e19244e01b881114553b56f0bc3f21d984e15896
badzia <m.badzi@proton.me>
2024-12-03 21:38:17 +0100

    correction nom docker image

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Variables
 DOCKER_REPO="badzim/codelands"
-
+APP_NAME="dpr-server" # Nom de votre application
 VERSION=${1:-"0.0"} # Utilise le paramètre passé ou "latest" par défaut
 
 echo "=================================================="
@@ -14,20 +14,20 @@ mvn clean package
 
 # Construire et pousser les images
 echo "🛠️  Building API image version $VERSION..."
-docker build -t $DOCKER_REPO:dpr-api-v$VERSION -f ./docker/Dockerfile .
+docker build -t $DOCKER_REPO:$APP_NAME-v$VERSION -f ./docker/Dockerfile .
 
 echo "🏷️  Tagging API image as latest..."
-docker tag $DOCKER_REPO:dpr-api-v$VERSION $DOCKER_REPO:dpr-api-latest
+docker tag $DOCKER_REPO:$APP_NAME-v$VERSION $DOCKER_REPO:$APP_NAME-latest
 
 echo "📤 Pushing API image version $VERSION..."
-docker push $DOCKER_REPO:dpr-api-v$VERSION
+docker push $DOCKER_REPO:$APP_NAME-v$VERSION
 
 echo "📤 Pushing API image as latest..."
-docker push $DOCKER_REPO:dpr-api-latest
+docker push $DOCKER_REPO:$APP_NAME-latest
 
 # Nettoyage
 echo "🧹 Cleaning up local images..."
-docker rmi $DOCKER_REPO:dpr-api-v$VERSION $DOCKER_REPO:dpr-api-latest || true
+docker rmi $DOCKER_REPO:$APP_NAME-v$VERSION $DOCKER_REPO:$APP_NAME-latest || true
 
 echo "🧹 Cleaning up local target..."
 rm -rfd ./target
