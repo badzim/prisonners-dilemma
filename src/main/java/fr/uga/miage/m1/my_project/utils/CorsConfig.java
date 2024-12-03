8be264f5f88a7585413b85bf56f0329e81fad79b
badzia <m.badzi@proton.me>
2024-12-03 22:26:06 +0100

    correction cors

--- src/main/java/fr/uga/miage/m1/my_project/utils/CorsConfig.java
+++ src/main/java/fr/uga/miage/m1/my_project/utils/CorsConfig.java
@@ -15,8 +15,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permet CORS sur tous les endpoints
                         .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://client.dpr.codelands.me/"
+                                "http://localhost:8002",
+                                "https://localhost:8002",
+                                "https://client.dpr.codelands.me",
+                                "https://api.dpr.codelands.me"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
